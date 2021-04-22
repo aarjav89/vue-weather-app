ce1bb7f44e24578b411aa518be16ab6ef92cfd77
aarjav89 <aarjav.chandarana@gmail.com>
2021-04-22 15:46:47 -0400

    Finished API Call & Displaying Fields

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main !='undefined' && weather.main.temp > 16 ? 'warm' : ''">
     <main>
       <div class="search-box">
-        <input type="text" name="" class="search-bar" placeholder="Search..."/>
+        <input type="text"
+               name=""
+               class="search-bar"
+               placeholder="Search..."
+               v-model="query"
+               @keypress="fetchWeather"/>
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">
-            Brampton, ON, Canada
+            {{ weather.name }}, {{weather.sys.country }}
           </div>
-          <div class="date">Tuesday 20th April 2021</div>
+          <div class="date">{{  dateBuilder() }}</div>
         </div>
 
         <div class="weather-box">
-          <div class="temp">9°C</div>
-          <div class="weather">Rain</div>
+          <div class="temp">{{ Math.round(weather.main.temp) }}°C</div>
+          <div class="weather">{{  weather.weather[0].main }}</div>
         </div>
 
       </div>
@@ -32,9 +37,39 @@ export default {
   },
   data() {
     return {
-      api_key: 'a5c80aab0d00463da7a8f32a633a4c6b',
+      api_key: '',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
 
     }
+  },
+  methods:{
+    fetchWeather(e){
+      if(e.key=="Enter"){
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+            .then(res=> {
+              return res.json();
+            }).then(this.setResults);
+      }
+    },
+    setResults(results){
+      this.weather=results;
+    },
+    dateBuilder() {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
+
+    }
+
   }
 }
 </script>
